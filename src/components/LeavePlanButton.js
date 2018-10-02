e5886f6593b88bef7ce8b70299da0c41201295f3
eckmLJE <lucas.eckman@gmail.com>
2018-10-02 15:19:32 -0400

    plan creators cannot leave

--- src/components/LeavePlanButton.js
+++ src/components/LeavePlanButton.js
@@ -6,18 +6,26 @@ import { removePlanUser } from "../actions/plans";
 const LeavePlanButton = props => {
   return (
     <div className="leave-plan">
-      <button onClick={() => props.removePlanUser(props.plan.id)}>
-        Leave Plan
-      </button>
+      {props.plan.attributes["creator-id"] !== props.currentUser.id ? (
+        <button onClick={() => props.removePlanUser(props.plan.id)}>
+          Leave Plan
+        </button>
+      ) : (
+        <p>(plan creators cannot leave)</p>
+      )}
     </div>
   );
 };
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
 
 const mapDispatchToProps = dispatch => ({
   removePlanUser: planId => dispatch(removePlanUser(planId))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(LeavePlanButton);
